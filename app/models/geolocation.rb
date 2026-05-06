8889a5cdea783eaf68f2a4c743249022c8270924
Hamza Fayyaz <hamza.fayyaz.devx@gmail.com>
2026-05-06 22:15:10 +0500

    fixed lint with rubocop

--- app/models/geolocation.rb
+++ app/models/geolocation.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require "uri"
-require "ipaddr"
+require 'uri'
+require 'ipaddr'
 
 class Geolocation < ApplicationRecord
   PROVIDERS = %w[ipstack null].freeze
@@ -23,9 +23,9 @@ class Geolocation < ApplicationRecord
   private
 
   def ip_or_url_present
-    if ip.blank? && url.blank?
-      errors.add(:base, "Either ip or url must be present")
-    end
+    return unless ip.blank? && url.blank?
+
+    errors.add(:base, 'Either ip or url must be present')
   end
 
   def ip_must_be_valid_address
@@ -33,7 +33,7 @@ class Geolocation < ApplicationRecord
 
     IPAddr.new(ip)
   rescue IPAddr::InvalidAddressError
-    errors.add(:ip, "must be a valid IP address")
+    errors.add(:ip, 'must be a valid IP address')
   end
 
   def normalize_lookup_key
@@ -44,8 +44,8 @@ class Geolocation < ApplicationRecord
     return if url.blank?
 
     uri = URI.parse(url)
-    errors.add(:url, "must be valid") unless uri.host.present?
+    errors.add(:url, 'must be valid') unless uri.host.present?
   rescue URI::InvalidURIError
-    errors.add(:url, "must be valid")
+    errors.add(:url, 'must be valid')
   end
 end
